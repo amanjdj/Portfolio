76a4e3ca027fb99e96e33220f56c61dedb3d2b4b
amanjdj <amanjdj@outlook.com>
2024-11-01 18:11:34 +0530

    Correction

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,6 @@ function NavBar() {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
-        {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="https://rahulkarda.netlify.app">Rahul</a></h1> */}
         <h1 className="text-5xl font-signature ml-2">
           <a
             className="link-underline link-underline-black"
@@ -39,7 +38,7 @@ function NavBar() {
             target="_blank"
             rel="noreferrer"
           >
-            Aman
+            Aman Sharma
           </a>
         </h1>
       </div>
